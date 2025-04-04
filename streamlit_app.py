672066ee1fbde31bc89cad2837cad1007c3fd904
Pratik Kaprekar <87488814+Protik-k@users.noreply.github.com>
2025-04-04 07:04:38 +0530

    Update streamlit_app.py
    adding smoothifroot to multi-select

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 # Write directly to the app
 st.title(" :cup_with_straw: Customise your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits your want in custom smoothie!")
@@ -21,6 +22,9 @@ if ingredient_lists:
     
     for fruit_chosen in ingredient_lists:
         ingredients_string += fruit_chosen +" "
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# st.text(smoothiefroot_response.json())
+        sfroot_df= st.dataframe(data=smoothiefroot_response.json(), use_container_width=True) 
         
     st.write(ingredients_string)
     
@@ -35,10 +39,8 @@ if ingredient_lists:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_smoothie+'!', icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sfroot_df= st.dataframe(data=smoothiefroot_response.json(), use_container_width=True) 
+
+
        
 
         
